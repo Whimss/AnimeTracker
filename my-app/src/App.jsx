2bf5adb64f1c1adf476e76424ab8c5bf4d92cd7a
Whimss <sanchez.alondra7@gmail.com>
2025-04-08 21:11:13 -0700

    adding
    added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,25 @@
 import './index.scss';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Results from './pages/Results';
+import SingleView from './pages/SingleView';
+import MainNavigation from './components/MainNavigation'
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+  <Router>
+    <MainNavigation/>
+    <main>
+    <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/single-view" element={<SingleView />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    </main>
+  </Router>
+  );
 }
 
-export default App
+export default App;
