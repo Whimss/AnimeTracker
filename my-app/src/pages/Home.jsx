eaff568354ccd4f0bd464e789e28a285bbfe2d2e
Whimss <sanchez.alondra7@gmail.com>
2025-04-13 17:55:17 -0700

    home/results
    set up home page and got results page to work

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,16 +1,40 @@
-import React,{useContext, useEffect} from 'react';
+import React, { useContext, useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 import { SearchContext } from '../context/search';
+import { FormControl, Input, IconButton, Grid } from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
+import './Home.scss'
 
 const Home = () => {
-    const search =useContext(SearchContext)
-    useEffect(() => {
-        search.search('Naruto').then((data)=>{
-            console.log(data);
+    const navigate =useNavigate();
+    const search = useContext(SearchContext)
+    const [input,setInput]=useState('');
 
+    const handleSearch =(event) => {
+        event.preventDefault();
+        search.search(input).then((data) => {
+        search.setData(data.results);
+        localStorage.setItem('myData',JSON.stringify(data.results));
+        navigate('/results');
         });
-
-    }, [search])
-    return (<div>Home</div>);
+    }
+    return (
+        <Grid container direction="column" justifyContent="center" alignItems="center">
+        <div>
+          <img alt="ichigo" src={import.meta.env.BASE_URL + 'ichigo.png'} height="420" width="auto" />
+        </div>
+        <div>
+          <form onSubmit={handleSearch} className='home_form'>
+            <FormControl className='home_formControl'>
+                <Input placeholder="Search for your favorite anime..." value={input} onChange={(event) => setInput(event.target.value)} className='home_input'/>
+                <IconButton variant="container" color="primary" type="submit" disabled={!input} onClick={handleSearch} className='home_iconButton'>
+                    <SearchIcon/>
+                </IconButton>
+            </FormControl>
+          </form>
+        </div>
+      </Grid>
+    );
 };
 
 export default Home;
--- my-app/src/pages/Results.jsx
+++ my-app/src/pages/Results.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useEffect,useContext,useState}from 'react';
+import { SearchContext } from '../context/search';
 
 const Results = () => {
-    return (<div>Results</div>);
+    const search = useContext(SearchContext);
+    const [dataExists, setDataExists] = useState(true)
+    useEffect (() => {
+        if (!search.animeData || search.animeData.length === 0){
+            try {
+                const storedData = JSON.parse(localStorage.getItem('myData'));
+                if (storedData && storedData.length > 0) {
+                    search.setData(storedData);
+                    setDataExists(true);
+                  } else {
+                    setDataExists(false);
+                  }
+               
+                
+                setDataExists(true);
+            } catch (error) {
+                console.log(error);
+                setDataExists(false);
+            }
+        }
+        console.log(search.anime);
+    }, [search]);
+    return (<div>
+        {(dataExists && 'Data Exists') || 'Data Does Not Exist'}
+    </div>);
 };
 
 export default Results;
